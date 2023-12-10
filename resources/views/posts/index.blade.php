ee7d82c855fdd982ae204f7b46e5463a03dbea59
AmongMember <bumeng873@gmail.com>
2023-12-10 16:14:58 +0900

    カリキュラム0806

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,24 +1,36 @@
 <!DOCTYPE html>
 <html lang="{{str_replace('_','-', app()->getLocale()) }}">
     <head>
-        <meta cherset="utf-8">
+        <meta charset="utf-8">
         <title>Blog</title>
         <!-- Fonts -->
-        <link href="http://fonts.googleapis.com/css?famiry=Nunito:200:600" rel="stylesheet">
+        <link href="http://fonts.googleapis.com/css?famiry=Nunito:200,600" rel="stylesheet">
     </head>
     <body class="antialiased">
         <h1>Blog Name</h1>
+        <a href='/posts/create'>create</a>
         <div class='posts'>
             @foreach ($posts as $post)
                 <div class='post'>
                     <a href="/posts/{{ $post->id }}"><h2 class='title'>{{$post->title}}</h2></a>
                     <p class='body'>{{$post->body}}</p>
-                    <a href='/posts/create'>create</a>
+                    <form action ="/posts/{{$post->id}}" id="form_{{ $post->id }}" method="post">
+                        @csrf
+                        @method('DELETE')
+                        <button type="button" onclick="deletePost({{ $post->id }})">delete</button>
+                    </form> 
                 </div>
             @endforeach
         </div>
-        <div class='paginate'>
-            {{$posts->links()}}
-        </div>
+        <div class='paginate'>{{$posts->links()}}</div>
+        <script>
+            function deletePost(id){
+                'use strict'
+                
+                if(confirm('削除すると復元できません。\n本当に削除しますか？')){
+                    document.getElementById(`form_${id}`).submit();
+                }
+            }
+        </script>
     </body>
 </html>
