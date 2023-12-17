2d190a85002a3c0d766b77d605c7156562fb0fd2
AmongMember <bumeng873@gmail.com>
2023-12-17 18:36:19 +0900

    カリキュラム0903

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -6,32 +6,39 @@
         <!-- Fonts -->
         <link href="http://fonts.googleapis.com/css?famlry=Nunito:200,600" rel="stylesheet">
     </head>
-    <body class="antialiased">
-        <h1>Blog Name</h1>
-        <a href='/posts/create'>[create]</a>
-        <div class='posts'>
-            @foreach ($posts as $post)
-                <div class='post'>
-                    <a href="/posts/{{ $post->id }}"><h2 class='title'>{{$post->title}}</h2></a>
-                    <a href="/categories/{{$post->category->id}}">{{$post->category->name}}</a>
-                    <p class='body'>{{$post->body}}</p>
-                    <form action ="/posts/{{$post->id}}" id="form_{{ $post->id }}" method="post">
-                        @csrf
-                        @method('DELETE')
-                        <button type="button" onclick="deletePost({{ $post->id }})">delete</button>
-                    </form> 
-                </div>
-            @endforeach
-        </div>
-        <div class='paginate'>{{$posts->links()}}</div>
-        <script>
-            function deletePost(id){
-                'use strict'
-                
-                if(confirm('削除すると復元できません。\n本当に削除しますか？')){
-                    document.getElementById(`form_${id}`).submit();
+    <x-app-layout>
+        <!--<x-slot name="header">-->
+        <!--    ヘッダー名-->
+        <!--</x-slot>-->
+        <body class="antialiased">
+            <h1>Blog Name</h1>
+            <a href='/posts/create'>[create]</a>
+            <div class='posts'>
+                @foreach ($posts as $post)
+                    <div class='post'>
+                        <a href="/posts/{{ $post->id }}"><h2 class='title'>{{$post->title}}</h2></a>
+                        <a href="/categories/{{$post->category->id}}">{{$post->category->name}}</a>
+                        <p class='body'>{{$post->body}}</p>
+                        <form action ="/posts/{{$post->id}}" id="form_{{ $post->id }}" method="post">
+                            @csrf
+                            @method('DELETE')
+                            <button type="button" onclick="deletePost({{ $post->id }})">delete</button>
+                        </form> 
+                    </div>
+                    <br>
+                @endforeach
+            </div>
+            ログインユーザー：{{ Auth::user()->name }}
+            <div class='paginate'>{{$posts->links()}}</div>
+            <script>
+                function deletePost(id){
+                    'use strict'
+                    
+                    if(confirm('削除すると復元できません。\n本当に削除しますか？')){
+                        document.getElementById(`form_${id}`).submit();
+                    }
                 }
-            }
-        </script>
-    </body>
+            </script>
+        </body>
+    </x-app-layout>
 </html>
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -7,22 +7,27 @@
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
     </head>
-    <body>
-        <h1 class="antialiased">
-            {{ $post->title }}
-        </h1>
-        <div class='edit'>
-            <a href="/posts/{{ $post->id }}/edit">[edit]</a>
-        </div>
-        <div class="content">
-            <div class="content__post">
-                <h3>本文</h3>
-                <p class='body'>{{ $post->body }}</p>    
+    <x-app-layout>
+        <!--<x-slot name="header">-->
+        <!--    ヘッダー名-->
+        <!--</x-slot>-->
+        <body>
+            <h1 class="antialiased">
+                {{ $post->title }}
+            </h1>
+            <div class='edit'>
+                <a href="/posts/{{ $post->id }}/edit">[edit]</a>
             </div>
-        </div>
-        <a href="/categories/{{$post->category->id}}">{{$post->category->name}}</a>
-        <div class='footer'>
-            <a href="/">戻る</a>
-        </div>
-    </body>
+            <div class="content">
+                <div class="content__post">
+                    <h3>本文</h3>
+                    <p class='body'>{{ $post->body }}</p>    
+                </div>
+            </div>
+            <a href="/categories/{{$post->category->id}}">{{$post->category->name}}</a>
+            <div class='footer'>
+                <a href="/">戻る</a>
+            </div>
+        </body>
+    </x-app-layout>
 </html>
